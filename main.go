d6ff9676c820aa080dadcf95dafb656d93df9c38
Nelson LeDuc <Nelson@Nelsons-Mac-mini.local>
2015-07-16 02:16:44 -0500

    Start working on database logic

--- main.go
+++ main.go
@@ -4,12 +4,26 @@ import (
 	"log"
 	"net/http"
     "os"
+    "database/sql"
 )
 
 func main() {
 	var g GMHook
-	err := http.ListenAndServe(":"+os.Getenv("PORT"), g)
+    err := http.ListenAndServe(GetPort(), g)
 	if err != nil {
 		log.Fatal(err)
 	}
+    
+    dbUrl := os.Getenv("DATABASE_URL")
+    database, _ := sql.Open("postgres", dbUrl)
+    log.Fatal(database)
+}
+
+func GetPort() string {
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "4000"
+    }
+    
+    return ":" + port
 }
