50c1804b294cfdebf84285c312124583867d3e34
Nelson LeDuc <Nelson@Nelsons-Mac-mini.local>
2015-07-16 02:10:12 -0500

    Make sure to bind to the correct port

--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"log"
 	"net/http"
+    "os"
 )
 
 func main() {
 	var g GMHook
-	err := http.ListenAndServe("localhost:4000", g)
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), g)
 	if err != nil {
 		log.Fatal(err)
 	}
