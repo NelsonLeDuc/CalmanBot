33242b62a56e4f0d136016994f007461fec64033
Nelson LeDuc <Nelson@Nelsons-Mac-mini.local>
2015-07-16 02:44:37 -0500

    Begin work on static pages

--- main.go
+++ main.go
@@ -4,19 +4,29 @@ import (
 	"log"
 	"net/http"
     "os"
-    "database/sql"
+//    "database/sql"
+//    "fmt"
 )
+
+type LocalHandler struct {}
+
+func (l LocalHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    http.ServeFile(w, r, "html/addBot.html")
+}
 
 func main() {
 	var g GMHook
+    var l LocalHandler
+    
     err := http.ListenAndServe(GetPort(), g)
 	if err != nil {
 		log.Fatal(err)
 	}
     
-    dbUrl := os.Getenv("DATABASE_URL")
-    database, _ := sql.Open("postgres", dbUrl)
-    log.Fatal(database)
+    http.ListenAndServe("/addBot" + GetPort(), l)
+    
+//    dbUrl := os.Getenv("DATABASE_URL")
+//    database, _ := sql.Open("postgres", dbUrl)
 }
 
 func GetPort() string {
