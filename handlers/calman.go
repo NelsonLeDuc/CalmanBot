a4b22b46c5101ff7ef3a06d83a568fdabb9be750
Nelson LeDuc <Nelson@Nelsons-Mac-mini.local>
2015-07-20 20:14:42 -0500

    Add image validation for gif, jpg, and png

--- handlers/calman.go
+++ handlers/calman.go
@@ -2,11 +2,12 @@ package handlers
 
 import (
 	"bytes"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"github.com/nelsonleduc/calmanbot/handlers/models"
-	"io/ioutil"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"regexp"
@@ -127,12 +128,12 @@ func validateURL(u string, image bool) bool {
 
 		resp, err := http.Get(u)
 		defer resp.Body.Close()
-		
+
 		if err == nil && resp.StatusCode >= 200 && resp.StatusCode < 300 {
 			if image {
 				return validateImage(resp.Body)
 			}
-			
+
 			return true
 		} else {
 			return false
@@ -152,5 +153,39 @@ func updateAction(a *models.Action, text string) {
 
 //TODO: Move this out of here
 func validateImage(r io.Reader) bool {
+	buf := make([]byte, 8)
+	num, err := r.Read(buf)
+	if err != nil || num < 8 {
+		return false
+	}
+
+	gif := convertHexSlice([]string{"47", "49", "46"})
+	jpg := convertHexSlice([]string{"FF", "D8", "FF", "E0"})
+	png := convertHexSlice([]string{"89", "50", "4E", "47", "D", "A", "1A", "A"})
+
+	return byteSliceSubset(buf, gif) || byteSliceSubset(buf, jpg) || byteSliceSubset(buf, png)
+}
+
+//b is a subset of a
+func byteSliceSubset(a, b []byte) bool {
+	for i, el := range b {
+		if el != a[i] {
+			return false
+		}
+	}
+	
+	fmt.Printf("Image valid, bytes: %v\n", b)
 	return true
+}
+
+func convertHexSlice(s []string) []byte {
+	b := []byte{}
+	for _, hexStr := range s {
+		result, err := hex.DecodeString(hexStr)
+		if err == nil {
+			b = append(b, result[0])
+		}
+	}
+
+	return b
 }
