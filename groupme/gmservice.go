0439b77b6abe84cfd9ec44434cf4718df9d9ca94
Nelson LeDuc <wolfon33@gmail.com>
2016-02-09 17:35:19 -0600

    Implement splitting up messages that are longer than 1000 characters long

--- groupme/gmservice.go
+++ groupme/gmservice.go
@@ -1,41 +1,50 @@
 package groupme
 
 import (
-	"encoding/json"
-	"net/http"
 	"bytes"
+	"encoding/json"
 	"io"
+	"net/http"
 	"time"
-	
+
 	"github.com/nelsonleduc/calmanbot/service"
+	"github.com/nelsonleduc/calmanbot/utility"
 )
 
 const postDelayMilliseconds = 500
+const groupmeLengthLimit = 1000
 
 func init() {
 	service.AddService("groupme", gmService{})
 }
 
-type gmService struct {}
+type gmService struct{}
 
 func (g gmService) PostText(key, text string) {
-	postBody := map[string]string{
-		"bot_id": key,
-		"text":   text,
-	}
 
-	encoded, err := json.Marshal(postBody)
-	if err != nil {
-		return
+	dividedText := utility.DivideString(text, groupmeLengthLimit)
+
+	for _, subText := range dividedText {
+		go func(key, message string) {
+			postBody := map[string]string{
+				"bot_id": key,
+				"text":   text,
+			}
+
+			encoded, err := json.Marshal(postBody)
+			if err != nil {
+				return
+			}
+
+			postToGroupMe(encoded)
+		}(key, subText)
 	}
-	
-	go postToGroupMe(encoded)
 }
 
 func (g gmService) MessageFromJSON(reader io.Reader) service.Message {
 	message := new(gmMessage)
 	json.NewDecoder(reader).Decode(message)
-	
+
 	return *message
 }
 
