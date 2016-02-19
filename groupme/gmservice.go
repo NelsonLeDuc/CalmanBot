7edae2894563b25b83066b007fb5656a66aab32d
Nelson LeDuc <wolfon33@gmail.com>
2016-02-18 23:42:47 -0600

    Implement pastebin for super long text posts

--- groupme/gmservice.go
+++ groupme/gmservice.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"os"
 	"time"
 
@@ -27,9 +28,17 @@ type gmService struct{}
 func (g gmService) PostText(key, text string, cacheID int, groupMessage service.Message) {
 
 	dividedText := utility.DivideString(text, groupmeLengthLimit)
+	if len(dividedText) > 2 {
+		pasteBinURL := postToPastebin(text)
+		if len(pasteBinURL) == 0 {
+			return
+		}
+
+		dividedText = []string{pasteBinURL}
+	}
 
 	for _, subText := range dividedText {
-		func(key, message string) {
+		go func(key, message string) {
 			postBody := map[string]string{
 				"bot_id": key,
 				"text":   message,
@@ -49,6 +58,49 @@ func (g gmService) PostText(key, text string, cacheID int, groupMessage service.
 	}
 
 	go updateLikes()
+}
+
+func (g gmService) MessageFromJSON(reader io.Reader) service.Message {
+	message := new(gmMessage)
+	json.NewDecoder(reader).Decode(message)
+
+	return *message
+}
+
+func (g gmService) ServiceMonitor() (service.Monitor, error) {
+	return GroupmeMonitor{}, nil
+}
+
+func postToPastebin(text string) string {
+	data := url.Values{}
+
+	data.Set("api_dev_key", os.Getenv("pastebinKey"))
+	data.Set("api_option", "paste")
+	data.Set("api_paste_code", text)
+
+	data.Set("api_paste_name", "Too long for /r/shitjustinsays")
+	data.Set("api_paste_private", "0")
+	data.Set("api_paste_expire_date", "1D")
+
+	resp, err := http.PostForm("http://pastebin.com/api/api_post.php", data)
+	if err != nil || resp.StatusCode != 200 {
+		return ""
+	}
+	defer resp.Body.Close()
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return ""
+	}
+
+	return string(respBody)
+}
+
+func postToGroupMe(body []byte) {
+	time.Sleep(postDelayMilliseconds * time.Millisecond)
+
+	postURL := "https://api.groupme.com/v3/bots/post"
+	http.Post(postURL, "application/json", bytes.NewReader(body))
 }
 
 type gmMessageWrapper struct {
@@ -75,22 +127,4 @@ func messageID(message service.Message) (string, error) {
 	}
 
 	return "", errors.New("No messages found")
-}
-
-func (g gmService) MessageFromJSON(reader io.Reader) service.Message {
-	message := new(gmMessage)
-	json.NewDecoder(reader).Decode(message)
-
-	return *message
-}
-
-func (g gmService) ServiceMonitor() (service.Monitor, error) {
-	return GroupmeMonitor{}, nil
-}
-
-func postToGroupMe(body []byte) {
-	time.Sleep(postDelayMilliseconds * time.Millisecond)
-
-	postURL := "https://api.groupme.com/v3/bots/post"
-	http.Post(postURL, "application/json", bytes.NewReader(body))
 }
