0eee722d23f9ab4d368f35a8f36b60aea6411076
Nelson LeDuc <nleduc@apple.com>
2020-05-18 23:08:49 -0700

    Update version to v2.11.5

--- handlers/builtin.go
+++ handlers/builtin.go
@@ -12,7 +12,7 @@ import (
 	"github.com/nelsonleduc/calmanbot/service"
 )
 
-const currentCalmanBotVersion string = "v2.11.4"
+const currentCalmanBotVersion string = "v2.11.5"
 
 type builtinDescription struct {
 	trigger     string
